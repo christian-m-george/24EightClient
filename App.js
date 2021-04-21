29a47349d42b0008c7a034f3243fe4aa4cfbe260
christian george <christian.george360@gmail.com>
2021-04-21 19:28:58 -0400

    commiting changes

--- App.js
+++ App.js
@@ -1,37 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, Image, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, Image, SafeAreaView, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableHighlight onPress={console.log(logHello)} >
+      <TouchableOpacity onPress={getHelloWorld}>
+
         <Image
           source={require('./assets/logo_248_transparent.png')}
           style={{height: 300, width: 300, resizeMode: 'contain'}}
         />
-      </TouchableHighlight>
+      </TouchableOpacity>
+
         <Text>Sign In</Text>
         <Text>Sign Up</Text>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </ SafeAreaView>
   );
 }
 
-const logHello = () => {
-  console.log('hello')
-}
-
-// const getHelloWorld = () => {
-//   return fetch('http://localhost:8000')
-//   .then((response) => response.json())
-//   .then((json) => {
-//     return json
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   })
+// const logHello = () => {
+//   console.log('hello')
 // }
+
+const getHelloWorld = () => {
+  return fetch('192.168.0.137:8000')
+  .then((response) => response.json())
+  .then((json) => {
+    return json
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+}
 
 const styles = StyleSheet.create({
   container: {
