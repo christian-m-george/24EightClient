fc46baf14f3ace427e857ae2a14c2bfb245e7650
christian george <christian.george360@gmail.com>
2021-04-21 15:13:12 -0400

    hello world

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Image, TouchableHighlight } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Image 
-        source={require('./assets/logo_248_transparent.png')}
-        style={{height: 300, width: 300, resizeMode: 'contain'}}
-      ></Image>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableHighlight onPress={console.log(logHello)} >
+        <Image
+          source={require('./assets/logo_248_transparent.png')}
+          style={{height: 300, width: 300, resizeMode: 'contain'}}
+        />
+      </TouchableHighlight>
+        <Text>Sign In</Text>
+        <Text>Sign Up</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
+
+const logHello = () => {
+  console.log('hello')
+}
+
+// const getHelloWorld = () => {
+//   return fetch('http://localhost:8000')
+//   .then((response) => response.json())
+//   .then((json) => {
+//     return json
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   })
+// }
 
 const styles = StyleSheet.create({
   container: {
